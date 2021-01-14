3fcf5ab7f672b816417f664562f8a82baba1d1ba
ahmadraza100 <ahmadrazashafi@gmail.com>
2021-01-15 00:02:35 +0500

    T3-Functions Added

---
+++ T3-Function.js
@@ -0,0 +1,46 @@
+console.log("Hello");
+
+// ----------------<Function>---------------
+
+// A function defination also called function declarations
+// or function statement
+
+// consits of function keyword , followed by
+
+//the name of function d
+
+
+// function sum(){
+//     var a=10 , b=30 ;
+//     var total = a+b; 
+//     return total;
+// }
+// console.log(sum());
+// console.log(`The sum is  ${sum()}`);
+
+
+// function mul(a ,b){
+//     console.log(`The Mul is ${a*b}`);
+
+// }
+// mul(10,20);
+
+// Arrow function
+
+// var number = 20 ; 
+
+// // default parameter
+
+// function mul(a ,b=5){
+//     console.log(`The Mul is ${a*b}`);
+
+// }
+// mul(10);
+
+
+// //Fat Arrow Function
+
+// let emi = () =>console.log("Ahmad");
+
+// emi();
+
